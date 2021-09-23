4934cb92e40d6dcb139a04cb5f0dfa21e704492c
Diego Koga <60438897+DK-diegokoga@users.noreply.github.com>
2021-09-23 20:31:23 -0300

    é isso ai

--- src/main/java/persistence/GrupoDaoImpl.java
+++ src/main/java/persistence/GrupoDaoImpl.java
@@ -31,21 +31,11 @@ public class GrupoDaoImpl implements IGrupoDao{
         CallableStatement cs = c.prepareCall(sql);
         cs.setInt(1, 1);
         //cs.registerOutParameter(2, Types.VARCHAR);
-         cs.execute();
-
-        
+        cs.execute();
         cs.close();
         
-        String sqlPQ = "SELECT * FROM Grupos";
-       
-        
-        PreparedStatement ps = c.prepareStatement(sqlPQ);
-		ResultSet rs = ps.executeQuery();
-		String saida = null ;
-		int aa = 0;
-		aa = rs.getInt("CodigoTime1");
-		
-        System.out.println("" + aa);
+        String saida = "Chamou aqui";
+        System.out.println(saida);
         return saida;
 	}
 
